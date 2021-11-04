5d81b7fcf13ae67d79852eede3a795779b010fb2
Leigh Wilson <code-tap@leighwilson.xyz>
2021-11-04 14:38:58 +0000

    Initial Commit
    Initial Commit - Program Working

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static IEnumerable<int> GenerateWithYield()
+        {
+            int index = 0;
+            while (true)
+                yield return index++;
+        }
+
+        public static void MethodWithThreadPool()
+        {
+        foreach ( int num in GenerateWithYield()){
+                ThreadPool.QueueUserWorkItem(new WaitCallback(state => StartBuzzer(num)));   
+            }           
+        }
+
+        public static void StartBuzzer(int num) {  
+                Console.WriteLine($"{num} :: {Buzzer(num)}");
+        }
+
+        public static string Buzzer(int i)
+        {
+            string msg = "";
+            if (i % 3 == 0) { msg += "Fizz"; }
+            if (i % 5 == 0) { msg += "Buzz"; }
+            return (msg);
+        }
+        static void Main(string[] args)
+        {
+            MethodWithThreadPool();
+        }
+    }
+}
